f346011151a2f99ce9cb35f3a4497d806b728d84
Michelle B <86215527+MichCS@users.noreply.github.com>
2021-09-22 01:37:16 +0000

    added notes to file GuessStarter.py week_01_python

--- week_01_python/GuessStarter.py
+++ week_01_python/GuessStarter.py
@@ -1,16 +1,20 @@
+#important python library called random
+
 import random
 
-
+#initialize difference
+#ask for input from user
 difference = 0
 print("I'm thinking of a number between 0 and a 100 (including both)...")
 myNumber = int(input("Select a number Between 0 and a 100 (including both): "))
 print("Can you guess my number?:")
 
+#initialize and declare at once (random integer)
 yourNumber = random.randint(0, 100)
 print(yourNumber)
 print("My Number is " + str(myNumber))
 
-
+#add if statements
 if (yourNumber > myNumber):
     difference = yourNumber - myNumber
 if (myNumber > yourNumber):
